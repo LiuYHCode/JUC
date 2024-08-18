f76a279a373c913186a2b3a8218ce8cd7fb1699a
LiuYHCode <lyh997835@163.com>
2024-08-18 23:44:57 +0800

    读写锁

---
+++ src/main/java/com/at2024/rw/ReadWriteLockDemo.java
@@ -0,0 +1,84 @@
+package com.at2024.rw;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author lyh
+ * @date 2024-08-18 23:34:34
+ */
+public class ReadWriteLockDemo {
+    public static void main(String[] args) {
+        MyCacheLock myCache = new MyCacheLock();
+
+        for (int i = 1; i <= 6; i++) {
+            final int temp = i;
+            new Thread(() -> {
+                myCache.put(temp+"", temp+"");
+            }, String.valueOf(i)).start();
+        }
+
+        // 读取
+        for (int i = 1; i <= 5 ; i++) {
+            final int temp = i;
+            new Thread(()->{
+                myCache.get(temp+"");
+            },String.valueOf(i)).start();
+        }
+    }
+}
+
+// 加锁的
+class MyCacheLock {
+    private volatile Map<String, Object> map = new HashMap<>();
+    // 读写锁： 更加细粒度的控制
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public void put(String key, Object value) {
+        readWriteLock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "写入" + key);
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "写入OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
+    }
+
+    public void get(String key) {
+        readWriteLock.readLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "读取" + key);
+            Object o = map.get(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+    }
+}
+
+/**
+ * 自定义缓存
+ */
+class MyCache{
+    private volatile Map<String,Object> map = new HashMap<>();
+
+    // 存，写
+    public void put(String key,Object value){
+        System.out.println(Thread.currentThread().getName()+"写入"+key);
+        map.put(key,value);
+        System.out.println(Thread.currentThread().getName()+"写入OK");
+    }
+
+    // 取，读
+    public void get(String key){
+        System.out.println(Thread.currentThread().getName()+"读取"+key);
+        Object o = map.get(key);
+        System.out.println(Thread.currentThread().getName()+"读取OK");
+    }
+}
