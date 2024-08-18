1072cc743f5c64a62b0d8916a25e9bf680ca4d85
LiuYHCode <lyh997835@163.com>
2024-08-18 16:15:25 +0800

    Lock版生产者消费者问题

--- src/main/java/com/at2024/pc/B.java
+++ src/main/java/com/at2024/pc/B.java
@@ -1,5 +1,9 @@
 package com.at2024.pc;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author lyh
  * @date 2024-08-18 15:32:18
@@ -8,9 +12,9 @@ package com.at2024.pc;
  * A num+1
  * B num-1
  */
-public class A {
+public class B {
     public static void main(String[] args) {
-        Data data = new Data();
+        Data2 data = new Data2();
         new Thread(() -> {
             for (int i = 0; i < 10; i++) {
                 try {
@@ -54,28 +58,45 @@ public class A {
 }
 
 // 判断等待，业务，通知
-class Data {
+class Data2 {
     private int num = 0;
 
-    public synchronized void increment() throws InterruptedException {
-        while (num != 0) { //使用if存在虚假唤醒，用while解决
-            //等待
-            this.wait();
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    public void increment() throws InterruptedException{
+        lock.lock();
+        try {
+            while (num != 0) { //使用if存在虚假唤醒，用while解决
+                //等待
+                condition.await();
+            }
+            num++;
+            // 通知其他线程，我+1完毕了
+            System.out.println(Thread.currentThread().getName()+"=>"+num);
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
-        num++;
-        // 通知其他线程，我+1完毕了
-        System.out.println(Thread.currentThread().getName()+"=>"+num);
-        this.notifyAll();
     }
 
-    public synchronized void decrement() throws InterruptedException {
-        while (num == 0) {
-            //等待
-            this.wait();
+    public void decrement() throws InterruptedException {
+        lock.lock();
+        try {
+            while (num == 0) {
+                //等待
+                condition.await();
+            }
+            num--;
+            System.out.println(Thread.currentThread().getName()+"=>"+num);
+            // 通知其他线程，我-1完毕了
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
-        num--;
-        System.out.println(Thread.currentThread().getName()+"=>"+num);
-        // 通知其他线程，我-1完毕了
-        this.notifyAll();
     }
 }
