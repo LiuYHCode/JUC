bb28549c732bac35b2c4f16ae165aa194546b532
LiuYHCode <lyh997835@163.com>
2024-08-20 23:47:11 +0800

    forkjoin以及stream并行流计算

---
+++ src/main/java/com/at2024/forkjoin/ForkJoinDemo.java
@@ -0,0 +1,43 @@
+package com.at2024.forkjoin;
+
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * @author lyh
+ * @date 2024-08-20 23:26:21
+ * 求和计算的任务！
+ * 3000   6000（ForkJoin）  9000（Stream并行流）
+ * 如何使用 forkjoin
+ * 1、forkjoinPool 通过它来执行
+ * 2、计算任务 forkjoinPool.execute(ForkJoinTask task)
+ * 3. 计算类要继承 ForkJoinTask
+ */
+public class ForkJoinDemo extends RecursiveTask<Long> {
+    private Long start;
+    private Long end;
+    private Long temp = 10000L;
+
+    public ForkJoinDemo(Long start, Long end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        if ((end-start) < temp) {
+            Long sum = 0L;
+            for (Long i = start; i <= end; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {// forkjoin 递归
+            long middle = (start + end) / 2; // 中间值
+            ForkJoinDemo task1 = new ForkJoinDemo(start, middle);
+            task1.fork(); // 拆分任务，把任务压入线程队列
+            ForkJoinDemo task2 = new ForkJoinDemo(middle+1, end);
+            task2.fork(); // 拆分任务，把任务压入线程队列
+
+            return task1.join() + task2.join();
+        }
+    }
+}
---
+++ src/main/java/com/at2024/forkjoin/Test.java
@@ -0,0 +1,51 @@
+package com.at2024.forkjoin;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.stream.LongStream;
+
+/**
+ * @author lyh
+ * @date 2024-08-20 23:37:44
+ * 同一个任务，别人效率高你几十倍！
+ */
+public class Test {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // test1(); // 8128
+        // test2(); // 11406
+        // test3(); // 126
+    }
+
+    // 普通程序员
+    public static void test1(){
+        Long sum = 0L;
+        long start = System.currentTimeMillis();
+        for (Long i = 1L; i <= 10_0000_0000; i++) {
+            sum += i;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("sum="+sum+" 时间："+(end-start));
+    }
+
+    // 会使用ForkJoin
+    public static void test2() throws ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        ForkJoinDemo task = new ForkJoinDemo(0L, 10_0000_0000L);
+        forkJoinPool.submit(task);
+        Long sum = task.get();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("sum="+sum+" 时间："+(end-start));
+    }
+
+    public static void test3(){
+        long start = System.currentTimeMillis();
+        // Stream并行流 ()  (]
+        long sum = LongStream.rangeClosed(0L, 10_0000_0000L).parallel().reduce(0, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println("sum="+sum+"时间："+(end-start));
+    }
+}
