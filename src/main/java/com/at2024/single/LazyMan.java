f52b6657ba46da5d54074ebe8a9e8164dc854998
LiuYHCode <lyh997835@163.com>
2024-08-22 21:22:47 +0800

    懒汉单例2.0，3.0优化

--- src/main/java/com/at2024/single/LazyMan.java
+++ src/main/java/com/at2024/single/LazyMan.java
@@ -8,15 +8,32 @@ package com.at2024.single;
  */
 public class LazyMan {
 
+    //v3.0加volatile避免指令重排
     private volatile static LazyMan lazyMan;
 
     private LazyMan() {
         System.out.println(Thread.currentThread().getName() + "ok!");
     }
 
+    //v2.0 双重检测锁模式的 懒汉式单例 DCL懒汉式，解决多线程情况下能拿到多个LazyMan实例
     private static LazyMan getInstance() {
         if (lazyMan == null) {
-            lazyMan = new LazyMan();
+            //锁的是Class类，只有一个
+            synchronized (LazyMan.class) {
+                if (lazyMan == null) {
+                    /**
+                     * 1. 分配内存空间
+                     * 2、执行构造方法，初始化对象
+                     * 3、把这个对象指向这个空间
+                     * 这里其实还是有风险的，new的时候要经过上面三步
+                     * 123
+                     * 132 A 指令重排出现问题，线程A还没有完成对象构造，初始化对象没有完成
+                     *     B // 此时lazyMan还没有完成构造，线程B直接返回了lazyMan
+                     */
+                    lazyMan = new LazyMan();
+                }
+
+            }
         }
         return lazyMan;
     }
